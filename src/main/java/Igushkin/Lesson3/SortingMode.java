e9e2b5d438dca7d84a0beb44c76dba2c7b57a07a
IgushkinV <mrbob86@yandex.ru>
2021-04-01 14:43:53 +0400

    Убран лишний код из SortingMode

--- src/main/java/Igushkin/Lesson3/SortingMode.java
+++ src/main/java/Igushkin/Lesson3/SortingMode.java
@@ -1,10 +1,7 @@
 package Igushkin.Lesson3;
 
 public enum SortingMode {
-    BY_ADD_ORDER("By add order"),
-    BY_KEY("By key"),
-    BY_VALUE("By value");
-
-    SortingMode(String string) {
-    }
+    BY_ADD_ORDER(),
+    BY_KEY(),
+    BY_VALUE();
 }
