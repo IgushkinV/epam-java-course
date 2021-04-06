1a003f2214740d8418ebd9e4b7b52bfe3af557f7
IgushkinV <mrbob86@yandex.ru>
2021-04-06 22:09:53 +0400

    Исправление замечаний к ДЗ №4. Заменил sout на лог. Добавил exception в лог для вывода стектрейса в логе. Добавил вывод сообщения при обработке исключений.

--- src/main/java/Igushkin/Lesson2/Main.java
+++ src/main/java/Igushkin/Lesson2/Main.java
@@ -15,11 +15,12 @@ public class Main {
         stringStorage.add("Three");
         stringStorage.add("AnotherOne");
         try {
-            System.out.println("Элемент хранилища c индексом 3: " + stringStorage.get(3));
+            log.info("Элемент хранилища c индексом 3: {}", stringStorage.get(3));
         } catch (NegativeIndexException e) {
-            e.printStackTrace();
+            log.warn("Обработка исключения NegativeIndexException при получении элемента хранилища", e);
         }
         stringStorage.clear();
+
         Storage<Integer> oneElemStorage = new Storage<>(new Integer[]{-1});
         //Удаление единственного элемента из хранилища. Теперь оно пустое.
         try {
@@ -31,36 +32,35 @@ public class Main {
         try {
             oneElemStorage.getLast();
         } catch (NegativeIndexException e) {
-            e.printStackTrace();
+            log.warn("Обработка исключения NegativeIndexException при получении последнего элемента хранилища", e);
         }
         //Удаление из пустого хранилища выбрасывает проверяемое исключение.
         try {
             oneElemStorage.delete();
         } catch (NegativeIndexException e) {
-            e.printStackTrace();
+            log.warn("Обработка исключения NegativeIndexException при удалении последнего элемента хранилища", e);
         }
 
         Storage<String> nameStorage = new Storage<>(new String[]{"Sara", "Ken", "Ada", "Bob", "Charly"});
         nameStorage.add("Elena");
         try {
-            System.out.println("Последний добавленный элемент: " + nameStorage.getLast());
+            log.info("Последний добавленный элемент: {}", nameStorage.getLast());
         } catch (NegativeIndexException e) {
-            e.printStackTrace();
+            log.warn("Обработка NegativeIndexException при получении последнего добавленного элемента", e);
         }
 
         Storage<Integer> numStorage = new Storage<>(new Integer[]{1,2,3,4,5});
         try {
-            System.out.println("Последний + первый элемент числового хранилища = " + (numStorage.getLast() + numStorage.get(0)));
+            log.info("Последний + первый элемент числового хранилища = {}", (numStorage.getLast() + numStorage.get(0)));
         } catch (NegativeIndexException e) {
-            e.printStackTrace();
+            log.warn("Обработка NegativeIndexException при получении элементов хранилища", e);
         }
         log.debug("Первое сообщение лог. Уровень дебаг.");
         //Метод, выбрасывающий проверяемое исключение. Оно обработано, программа выполняется дальше.
         try {
             nameStorage.get(-1);
         } catch (NegativeIndexException e) {
-            e.printStackTrace();
-            log.warn("Запрос элемента с отрицательным индексом");
+            log.debug("Обработка исключения при получении элемента хранилища.", e);
         }
         //Метод, выбрасывающий непроверяемое исключение. Программа не выполняется дальше.
         nameStorage.add(null);
@@ -68,7 +68,7 @@ public class Main {
         try {
             System.out.println(nameStorage.getLast());
         } catch (NegativeIndexException e) {
-            e.printStackTrace();
+            log.warn("Обратобка получения последнего элемента хранилища.", e);
         }
     }
 }
