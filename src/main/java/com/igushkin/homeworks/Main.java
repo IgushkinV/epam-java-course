8563ff7262ddf1d92b449d67cf73343f22e79f9d
IgushkinV <mrbob86@yandex.ru>
2021-04-30 00:12:55 +0400

    Three stars task solution.

--- src/main/java/com/igushkin/homeworks/Main.java
+++ src/main/java/com/igushkin/homeworks/Main.java
@@ -2,6 +2,7 @@ package com.igushkin.homeworks;
 
 
 import com.igushkin.homeworks.lesson9.AnnotationProcessor;
+import com.igushkin.homeworks.lesson9.annotations.Entity;
 import com.igushkin.homeworks.lesson9.exceptions.NoValueAnnotationException;
 import com.igushkin.homeworks.lesson9.fileUtilities.FileUtilities;
 import com.igushkin.homeworks.lesson9.pojoClasses.Human;
@@ -10,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,8 @@ public class Main {
     final static Logger log = LoggerFactory.getLogger(Main.class);
 
     private final static Path PATH = Path.of("src/main/resources/file.txt");
+
+    public final static String DIRECTORY = "target\\classes";
 
     public static void main(String[] args) {
         AnnotationProcessor processor = new AnnotationProcessor();
@@ -48,14 +52,13 @@ public class Main {
         for (Human human : humanList) {
             log.info("age={}, name={}", human.getAge(), human.getName());
         }
-
+        log.info("Three start task solution demonstration:");
+        int count = 0;
         try {
-            FileUtilities.countAnnotations("epam-java-course");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            count = FileUtilities.countAnnotations(DIRECTORY);
+        } catch (NotDirectoryException e) {
+            log.error("main() - error while trying to count annotations. Check the passed directory.", e);
         }
-        log.info("", 10);
+        log.info("main() - The directory {} has {} classes annotated with @Entity", DIRECTORY, count);
     }
 }
