6f8a232e5ce32a31531ab7ea68c268f41d4be8ff
IgushkinV <mrbob86@yandex.ru>
2021-05-01 18:13:59 +0400

    One star task solution. Class Sausage.java

---
+++ src/main/java/igushkin/homeworks/lesson10/task2/Sausage.java
@@ -0,0 +1,24 @@
+package igushkin.homeworks.lesson10.task2;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Optional;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Sausage implements Serializable {
+    private String type = null;
+
+    private int weight;
+
+    private long cost;
+
+    public Optional<String> getType() {
+        return Optional.ofNullable(type);
+    }
+
+}
