3092efacd77334c13835ffe19e53308895d08fc8
IgushkinV <mrbob86@yandex.ru>
2021-05-10 19:00:41 +0400

    Homework 12, Tests fix.

--- homework-impl/src/test/java/com/epam/igushkin/homework/MainTest.java
+++ homework-impl/src/test/java/com/epam/igushkin/homework/MainTest.java
@@ -1,4 +1,12 @@
 package com.epam.igushkin.homework;
 
+import org.junit.jupiter.api.Test;
+
 public class MainTest {
+
+    @Test
+    public void mainTest() {
+        Main.main(null);
+
+    }
 }
